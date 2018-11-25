8e6f3c54bae8863bba30d53ab8504b3cacea110f
regina <garaeva.regina9@gmail.com>
2018-11-26 00:56:06 +0500

    Add try-except block to TextFileWriter.

--- src/TextFileWriter.py
+++ src/TextFileWriter.py
@@ -3,10 +3,15 @@ import os
 
 class TextFileWriter:
     def write(self, file_path, text):
-        directory = os.path.dirname(file_path)
-        if directory:
-            if not os.path.exists(directory):
-                os.makedirs(directory)
+        try:
+            directory = os.path.dirname(file_path)
+            if directory:
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
 
-        with open(file_path, 'w', encoding='utf-8') as output_file:
-            output_file.write(text)
+            with open(file_path, 'w', encoding='utf-8') as output_file:
+                output_file.write(text)
+
+            print('File {0} saved'.format(file_path))
+        except Exception as e:
+            print('Can not save file {0} with error {1}'.format(file_path, e))
