f535c0fa763603074442a2aeed72faa40c58a833
regina <garaeva.regina9@gmail.com>
2018-11-25 15:20:03 +0500

    Check if given file path has no directory

--- src/TextFileWriter.py
+++ src/TextFileWriter.py
@@ -4,8 +4,9 @@ import os
 class TextFileWriter:
     def write(self, file_path, text):
         directory = os.path.dirname(file_path)
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+        if directory:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
 
         with open(file_path, 'w', encoding='utf-8') as output_file:
             output_file.write(text)
