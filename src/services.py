e31fa25b57cc02734ab1f8213b5d1e0bd6b8b086
regina <garaeva.regina9@gmail.com>
2018-11-26 15:05:03 +0500

    Try to use chars_per_punctuation_avg_count in ContentDefineService.

--- src/services.py
+++ src/services.py
@@ -6,21 +6,29 @@ from src.io import TextFileWriter
 
 
 class WeightElement:
-    sentences_count = None
-    sentences_avg_length = None
-    repeated_tags_count = None
-    non_text_tags_count = None
-    punctuation_count = None
-
     def __init__(self, elem):
-        self.element = elem
-        text = re.sub('[ \r\n\t]+', ' ', elem.text_content())
+        punctuation_pattern = '[,:‐–—;«»"\'()]'
+        sentence_pattern = '\s*[^.!?]+[.!?]'
 
-        self.sentences = [x.strip() for x in list(filter(None, text.split('.')))]
+        word_avg_length = 6.5
+        sentence_avg_length = 30
+        chars_per_punctuation_avg_count = 40
+
+        self.element = elem
+        self.text = re.sub('[ \r\n\t]+', ' ', elem.text_content())
+        self.sentences = [x.strip() for x in list(filter(None, re.findall(sentence_pattern, self.text)))]
         self.sentences_count = len(self.sentences)
         self.sentences_avg_length = sum(map(lambda x: len(x), self.sentences)) / len(self.sentences) if len(self.sentences) > 0 else 0
-        self.punctuation_count = len(re.findall(',:‐–—;«»"\'\(\)', text))
-        self.cost = self.sentences_count * 10 + self.sentences_avg_length * 0.05 + self.punctuation_count * 5
+        self.punctuation_count = len(re.findall(punctuation_pattern, self.text))
+        self.chars_per_punctuation = len(self.text) / (self.punctuation_count + 1)
+
+        self.param = 0
+        if chars_per_punctuation_avg_count > self.chars_per_punctuation:
+            self.param = chars_per_punctuation_avg_count / self.chars_per_punctuation if self.chars_per_punctuation else 1
+        else:
+            self.param = self.chars_per_punctuation / chars_per_punctuation_avg_count
+
+        self.cost = self.sentences_count * 10 + self.sentences_avg_length * 0.05 - (self.param - 1) * 100
 
 
 class ContentDefineService:
@@ -52,6 +60,10 @@ class ContentDefineService:
             temp += 'Sentences ' + str(e.sentences) + '\n\n'
             temp += 'Sentences count ' + str(e.sentences_count) + '\n\n'
             temp += 'Sentences avg length ' + str(e.sentences_avg_length) + '\n\n'
+            temp += 'Punctuation count ' + str(e.punctuation_count) + '\n\n'
+            temp += 'Text length ' + str(len(e.text)) + '\n\n'
+            temp += 'Chars Per Punctuation' + str(e.chars_per_punctuation) + '\n\n'
+            temp += 'Param' + str(e.param) + '\n\n'
             temp += 'Score ' + str(e.cost) + '\n\n'
             temp += e.element.text_content() + '\n\n'
             temp += '---\n\n'
