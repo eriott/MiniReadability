f73ca573d3b16e4cd4f7a4b682927036b85396cf
regina <garaeva.regina9@gmail.com>
2018-11-27 11:32:26 +0500

    Check parameters count in main.py. Raise exception when given url returns unsuccess status code.

--- main.py
+++ main.py
@@ -2,39 +2,41 @@ import sys
 
 import requests
 
-from src.parser import MiniReadabilityParser
-from src.io import TextFileWriter
 from src.converters import UrlToFilepathConverter
+from src.io import TextFileWriter
+from src.parser import MiniReadabilityParser
 from src.validators import UrlValidator
 
-if sys.argv[1] == '-f':
-    filename = sys.argv[2]
-    with open(filename) as f:
-        urls = [x.strip() for x in f.readlines()]
+if len(sys.argv) == 2:
+    url = sys.argv[1]
 else:
-    urls = [sys.argv[1]]
+    print('Wrong number of parameters.')
+    print('Usage: python main.py https://habr.com/company/mailru/blog/429186')
+    exit(1)
 
-for url in urls:
+try:
     if not (UrlValidator().is_valid(url)):
         print('Skip invalid URL', url)
-        continue
+        exit(1)
 
-    try:
-        print('Process URL', url)
-        page = requests.get(url)
-        encoding = page.encoding if page.encoding else 'utf-8'
+    print('Process URL', url)
+    page = requests.get(url)
+    page.raise_for_status()
+    encoding = page.encoding if page.encoding else 'utf-8'
 
-        if not page.content:
-            raise Exception('No content received from URL')
+    if not page.content:
+        raise Exception('No content received from URL')
 
-        output = MiniReadabilityParser().parse(page.content.decode(page.encoding), url)
+    output = MiniReadabilityParser().parse(page.content.decode(page.encoding), url)
 
-        file_path = UrlToFilepathConverter().convert(url)
-        TextFileWriter().write(file_path, output)
-        print()
-    except UnicodeDecodeError as e:
-        print('Can not decode page content with encoding', encoding)
-    except requests.exceptions.RequestException as e:
-        print('Can not get page.')
-    except Exception as e:
-        print('Error has occurred:', e)
+    file_path = UrlToFilepathConverter().convert(url)
+    TextFileWriter().write(file_path, output)
+    exit(0)
+except UnicodeDecodeError as e:
+    print('Can not decode page content with encoding', encoding)
+except requests.exceptions.RequestException as e:
+    print('Can not get page.', e)
+except Exception as e:
+    print('Error has occurred:', e)
+finally:
+    exit(1)
